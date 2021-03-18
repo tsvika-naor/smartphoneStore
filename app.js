0c3b3d50a854b98c3c41bef81e8fb5eae7bdcd21
tzvika-naor <zvinaor@gmail.com>
2021-03-18 14:41:00 +0200

    add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const socketIo = require('socket.io');
+// const socketIo = require('socket.io');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const orderRoutes = require('./server/routes/order');
@@ -17,63 +17,65 @@ mongoose.connect(process.env._MONGODB_URI || connectionString, { useNewUrlParser
     .catch(() => console.log('connection failed!!!'));
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/order", orderRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/smartphone", smartphoneRoutes);
-console.log(express.static("../server/client/react/")) 
+// console.log(express.static("../server/client/react/")) 
 
 
 const server = http.createServer(app);
 
-const io = socketIo(server/*, {
+// const io = socketIo(server
+    /*, {
     cors: {
         origins: ["http://localhost:4200", "http://localhost:3000"],
         methods: ["GET", "POST"],
         credentials: false
     }
-}*/);
+}*/
+// );
 
 //var ordersCounter = 0;
 var usersCounter = 0;
 //var smartphonesCounter = 0;
 
-io.on('connection', (socket) => {
-    console.log('New Connection!', socket.handshake.headers.origin)
+// io.on('connection', (socket) => {
+//     console.log('New Connection!', socket.handshake.headers.origin)
     //When user connected from react client (not Admin panel)
-    if (socket.handshake.headers.origin === "http://localhost:3000") {
-        console.log('New user connected!');
-        usersCounter++;
-        socket.broadcast.emit('usersCounter', usersCounter)
+    // if (socket.handshake.headers.origin === "http://localhost:3000") {
+    //     console.log('New user connected!');
+    //     usersCounter++;
+    //     socket.broadcast.emit('usersCounter', usersCounter)
 
-        socket.on('disconnect', () => {
-            console.log('User disconnected!');
-            usersCounter--;
-            socket.broadcast.emit('usersCounter', usersCounter)
-        });
-    }
+    //     socket.on('disconnect', () => {
+    //         console.log('User disconnected!');
+    //         usersCounter--;
+    //         socket.broadcast.emit('usersCounter', usersCounter)
+    //     });
+    // }
 
     //When Order is deleted or added
-    socket.on('changeOrdersCount', () => {
-        orderController.getOrdersCount(null, null, (ordersCount) => {
+    // socket.on('changeOrdersCount', () => {
+    //     orderController.getOrdersCount(null, null, (ordersCount) => {
             //ordersCounter = ordersCount;
-            socket.broadcast.emit('ordersCounter', ordersCount);
-            console.log('Total orders changed !!');
-        });
-    })
+    //         socket.broadcast.emit('ordersCounter', ordersCount);
+    //         console.log('Total orders changed !!');
+    //     });
+    // })
 
     //When Smartphone is deleted or added
-    socket.on('changeSmartphonesCount', () => {
-        smartphoneController.getSmartphonesCount(null, null, (smartphonesCount) => {
+    // socket.on('changeSmartphonesCount', () => {
+    //     smartphoneController.getSmartphonesCount(null, null, (smartphonesCount) => {
             //smartphonesCounter = smartphonesCount;
-            socket.broadcast.emit('smartphonesCounter', smartphonesCount);
-            console.log('Total smartphones changed !!');
-        });
-    })
+    //         socket.broadcast.emit('smartphonesCounter', smartphonesCount);
+    //         console.log('Total smartphones changed !!');
+    //     });
+    // })
 
-});
+// });
 console.log(process.env.PORT)
 const port = process.env.PORT || 5000
 
